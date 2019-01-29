3b8a004863e1c16b9770a9ed296426504203bbba
chrisdaaz <chris-diaz@northwestern.edu>
2019-01-29 15:26:52 -0600

    removes instructions

--- static/js/search.js
+++ static/js/search.js
@@ -22,7 +22,7 @@ if(searchQuery){
   $("#search-query").val(searchQuery);
   executeSearch(searchQuery);
 }else {
-  $('#search-results').append("<p>Please enter a word or phrase above</p>");
+  $('#search-results');
 }
 
 
